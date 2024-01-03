054e7f303fab7ed8ea8484433e7813d82ab3ef72
prince-automax <princethomas@autobse.com>
2024-01-03 17:40:25 +0530

    commented code removed

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretDown } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 import RecentlySold from "../components/home/RecentlySold";
-import Newsletter from "../components/home/Newsletter";
 import Advantages from "../components/home/Advantages";
 import LogoCloud from "../components/home/LogoCloud";
 import DelearBenefits from "../components/home/DelearBenefits";
@@ -39,62 +38,59 @@ export default function Example() {
   return (
     <>
       <HeroSection />
-      {/* <AllEventsTable/> */}
-      {/* /////////////////////////////////////////////////////////////////////////////////////////////////// */}
 
-   <div className="pl-6">
-   <div className="w-full  sm:px-0  my-2 p-4 ">
-        <Tab.Group>
-          <Tab.List className="flex justify-center space-x-1  sm:w-96  border ">
-            {Object.keys(tabs).map((tab, tabIndex) => (
-              <Tab
-                key={tab}
-                className={({ selected }) =>
-                classNames(
-                  "w-full h-full p-2 relative  text-sm font-medium leading-5",
-                  "ring-offset-0 focus:outline-none text-black ",
-                  selected
-                    ? "bg-blue-600 text-white"
-                    : "text-white bg-slate-500 ]"
-                )
-                }
-              >
-                {({ selected }) => (
-                  <>
-                    {tab}
-                    {selected && (
-                      <span className="absolute top-10 left-14 transform -translate-y-1/2 transition ease-in-out delay-100">
-                        {/* <FontAwesomeIcon icon={faCaretDown} className="w-10 h-8 text-black" />{" "} */}
-                        <FontAwesomeIcon icon={faCaretDown} className="w-20 h-8 text-blue-600 "/>
-                        
-                      </span>
-                    )}
-                  </>
-                )}
-              </Tab>
-            ))}
-          </Tab.List>
+      <div className="pl-6">
+        <div className="w-full  sm:px-0  my-2 p-4 ">
+          <Tab.Group>
+            <Tab.List className="flex justify-center space-x-1  sm:w-96  border ">
+              {Object.keys(tabs).map((tab, tabIndex) => (
+                <Tab
+                  key={tab}
+                  className={({ selected }) =>
+                    classNames(
+                      "w-full h-full p-2 relative  text-sm font-medium leading-5",
+                      "ring-offset-0 focus:outline-none text-black ",
+                      selected
+                        ? "bg-blue-600 text-white"
+                        : "text-white bg-slate-500 ]"
+                    )
+                  }
+                >
+                  {({ selected }) => (
+                    <>
+                      {tab}
+                      {selected && (
+                        <span className="absolute top-10 left-14 transform -translate-y-1/2 transition ease-in-out delay-100">
+                          {/* <FontAwesomeIcon icon={faCaretDown} className="w-10 h-8 text-black" />{" "} */}
+                          <FontAwesomeIcon
+                            icon={faCaretDown}
+                            className="w-20 h-8 text-blue-600 "
+                          />
+                        </span>
+                      )}
+                    </>
+                  )}
+                </Tab>
+              ))}
+            </Tab.List>
 
-          <Tab.Panels className="mt-6">
-            <Tab.Panel>
-              <UpcomingEventHomePage />
-            </Tab.Panel>
-            <Tab.Panel>
-              <LiveEventHomePage />
-            </Tab.Panel>
-          </Tab.Panels>
-        </Tab.Group>
+            <Tab.Panels className="mt-6">
+              <Tab.Panel>
+                <UpcomingEventHomePage />
+              </Tab.Panel>
+              <Tab.Panel>
+                <LiveEventHomePage />
+              </Tab.Panel>
+            </Tab.Panels>
+          </Tab.Group>
+        </div>
       </div>
-   </div>
-
-      {/* ///////////////////////////////////////////////////////////////////////////////////////////////////////// */}
 
       <Cards />
       <RecentlySold />
       <LogoCloud />
       <DelearBenefits />
       <Advantages />
-      {/* <Newsletter /> */}
     </>
   );
 }
